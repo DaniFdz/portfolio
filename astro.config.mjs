d393a400bc276d5a344818d6e880d89a896ce6fb
DaniFdz <danifernandezzzzzz@gmail.com>
2024-01-25 15:38:01 +0100

    Contact form

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,17 @@
 import { defineConfig } from 'astro/config';
-
 import tailwind from "@astrojs/tailwind";
+
+import preact from "@astrojs/preact";
+
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
-	vite: {
-		server: {
-			watch: {
-				usePolling: true,
-			}
-		}
-	}
+  integrations: [tailwind(), preact()],
+  vite: {
+    server: {
+      watch: {
+        usePolling: true
+      }
+    }
+  }
 });
